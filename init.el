fb24b972b70d6fe9dc180b311fc8b8e483808dfc
Droplan <axelm13@gmail.ru>
2023-11-11 22:03:29 +0400

    changed from video 1-3

--- init.el
+++ init.el
@@ -1,4 +1,20 @@
+;; Creates configuration file from .org file
 (org-babel-load-file
  (expand-file-name
   "config.org"
   user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(deeper-blue))
+ '(custom-safe-themes
+   '("dd078e6b1260f3dc1ba7dd562ce21443d62fc10cd31906e325bd3a7485ca9552" default))
+ '(package-selected-packages '(ivy-rich all-the-icons-ivy-rich all-the-icons)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

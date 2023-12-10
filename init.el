c9babdbfafeaf84a639311a1f58e3eb422565f26
Droplan <axelm13@gmail.ru>
2023-12-10 18:34:22 +0400

    Added 'ensure t' for all plagins, add variables for uppercase and lowercase

--- init.el
+++ init.el
@@ -2,16 +2,5 @@
  (expand-file-name
   "config.org"
   user-emacs-directory))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(all-the-icons-dired projectile diminish rainbow-mode magit atom-one-dark-theme which-key org-bullets toc-org flycheck company consult orderless vertico)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+

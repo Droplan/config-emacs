19bf19f88d3b71730780eb98434f72e99dfcdcb9
Droplan <axelm13@gmail.ru>
2023-12-13 23:14:07 +0400

    Config use-package, delete property "ensure t" of all plagins

--- init.el
+++ init.el
@@ -2,5 +2,16 @@
  (expand-file-name
   "config.org"
   user-emacs-directory))
-
-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(which-key vertico toc-org rainbow-mode rainbow-delimiters projectile org-bullets orderless markdown-mode magit lua-mode flycheck diminish dashboard consult company atom-one-dark-theme all-the-icons-dired)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

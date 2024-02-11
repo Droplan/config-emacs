1c3005086f5a5bcc62ca96a5bb4946838e776f8d
Droplan <axelm13@gmail.ru>
2024-02-11 21:23:28 +0400

    Add citar, citar-org-roam, embark

--- init.el
+++ init.el
@@ -7,6 +7,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(company-show-quick-access t nil nil "Customized with use-package company")
+ '(org-indent-indentation-per-level 4)
  '(package-selected-packages
    '(guess-language flyspell-correct olivetti which-key vertico toc-org rainbow-mode rainbow-delimiters projectile org-modern org-fragtog org-bullets org-appear orderless markdown-mode marginalia magit lua-mode flycheck diminish dashboard consult company-posframe atom-one-dark-theme all-the-icons-dired)))
 (custom-set-faces
@@ -15,3 +17,4 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'dired-find-alternate-file 'disabled nil)

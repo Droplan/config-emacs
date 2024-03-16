77dce9fee31521f1c493c67d284e037da63c6c31
Droplan <axelm13@gmail.ru>
2024-03-16 19:07:38 +0400

    Settings for agenda, add view for agenda

--- init.el
+++ init.el
@@ -2,6 +2,7 @@
  (expand-file-name
   "config.org"
   user-emacs-directory))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -9,11 +10,10 @@
  ;; If there is more than one, they won't work right.
  '(company-show-quick-access t nil nil "Customized with use-package company")
  '(package-selected-packages
-   '(guess-language flyspell-correct olivetti which-key vertico toc-org rainbow-mode rainbow-delimiters projectile org-modern org-fragtog org-bullets org-appear orderless markdown-mode marginalia magit lua-mode flycheck diminish dashboard consult company-posframe atom-one-dark-theme all-the-icons-dired)))
+   '(org-fancy-priorities which-key vertico toc-org rainbow-mode rainbow-delimiters projectile org-modern org-fragtog org-bullets org-appear orderless olivetti markdown-mode marginalia magit lua-mode guess-language flyspell-correct flycheck embark-consult diminish dashboard corfu company-posframe citar-org-roam citar-embark atom-one-dark-theme all-the-icons-dired)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-(put 'dired-find-alternate-file 'disabled nil)

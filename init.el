401f13a8f347f43b9535acc3abbb4b21ab3b6fac
Droplan <axelm13@gmail.ru>
2024-03-17 22:33:27 +0400

    Add org-agenda-files-track, but no work

--- init.el
+++ init.el
@@ -9,6 +9,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(company-show-quick-access t nil nil "Customized with use-package company")
+ '(org-agenda-files
+   '("/home/aleksey/Documents/notes/daily/2023_08_24.org" "/home/aleksey/Documents/notes/daily/2023_10_01.org" "/home/aleksey/Documents/notes/daily/2023_10_03.org" "/home/aleksey/Documents/notes/daily/2023_10_16.org" "/home/aleksey/Documents/notes/daily/2023_10_18.org"))
  '(package-selected-packages
    '(org-fancy-priorities which-key vertico toc-org rainbow-mode rainbow-delimiters projectile org-modern org-fragtog org-bullets org-appear orderless olivetti markdown-mode marginalia magit lua-mode guess-language flyspell-correct flycheck embark-consult diminish dashboard corfu company-posframe citar-org-roam citar-embark atom-one-dark-theme all-the-icons-dired)))
 (custom-set-faces

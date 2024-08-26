9e6ec0d6fae66597ca7050e148dca69b8498d874
Droplan <axelm13@gmail.ru>
2024-08-26 23:09:13 +0400

    Некоторые настройки для улучшения отображения в org-mod

--- init.el
+++ init.el
@@ -9,12 +9,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(company-show-quick-access t nil nil "Customized with use-package company")
- '(custom-safe-themes
-   '("1b374e40e63fdfe66378edb8f8a5d34e327822229a6a5905b3fbd0b4a71fcd51" default))
  '(org-agenda-files
    '("/home/aleksey/Documents/notes/daily/2023_08_24.org" "/home/aleksey/Documents/notes/daily/2023_10_01.org" "/home/aleksey/Documents/notes/daily/2023_10_03.org" "/home/aleksey/Documents/notes/daily/2023_10_16.org" "/home/aleksey/Documents/notes/daily/2023_10_18.org" "/home/aleksey/Documents/notes/daily/2024-03-19.org" "/home/aleksey/Documents/notes/daily/2024-04-14.org" "/home/aleksey/Documents/notes/dzheymichanPythonBystryyStart2021.org" "/home/aleksey/Documents/notes/финансы.org"))
  '(package-selected-packages
-   '(org-fancy-priorities which-key vertico toc-org rainbow-mode rainbow-delimiters projectile org-modern org-fragtog org-bullets org-appear orderless olivetti markdown-mode marginalia magit lua-mode guess-language flyspell-correct flycheck embark-consult diminish dashboard corfu company-posframe citar-org-roam citar-embark atom-one-dark-theme all-the-icons-dired)))
+   '(solarized-theme which-key vertico toc-org reverse-im rainbow-mode rainbow-delimiters projectile org-modern org-fragtog org-appear org-agenda-files-track orderless olivetti markdown-mode marginalia magit lua-mode guess-language flyspell-correct flycheck embark-consult diminish dashboard company-posframe citar-org-roam citar-embark all-the-icons-dired)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
